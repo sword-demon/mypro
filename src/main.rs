7ebf4c1083be5b51852ae29ed99af2ebf68b41a9
wxvirus <1418667580@qq.com>
2022-08-31 00:08:52 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let my_age:u8 = 2;
+    let your_age = my_age  +2;
+    println!("我的年龄是: {}", my_age);
+    println!("你的年龄是: {}", your_age);
+    println!("u8最大值: {}, 最小值{}", u8::max_value(), u8::min_value());
+}
