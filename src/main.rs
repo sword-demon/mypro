e87fd2c5a66a6980579120ff309e0b891501d72e
wxvirus <1418667580@qq.com>
2022-09-01 23:06:16 +0800

    初识函数和模块已经私有和公有关键字

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+mod lib{
+    pub fn show_me() {
+        let my_name = "wujie";
+        let my_age = 18;
+        println!("我的名字是: {}, 年龄是: {}", my_name, my_age);
+    }
+}
 fn main() {
-    let my_age:u8 = 2;
-    let your_age = my_age  +2;
-    println!("我的年龄是: {}", my_age);
-    println!("你的年龄是: {}", your_age);
     println!("u8最大值: {}, 最小值{}", u8::max_value(), u8::min_value());
+    lib::show_me();
 }
