8472fdff4b56ab4362ca33a73f34705b00c6bbf6
wxvirus <1418667580@qq.com>
2022-09-01 23:13:24 +0800

    同级目录加载mod

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod helper {
+	pub fn show_me() {
+		let my_name = "wujie";
+		let my_age = 18;
+		println!("我的名字是: {}, 年龄是: {}", my_name, my_age);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-mod lib{
-    pub fn show_me() {
-        let my_name = "wujie";
-        let my_age = 18;
-        println!("我的名字是: {}, 年龄是: {}", my_name, my_age);
-    }
-}
+mod lib;
 fn main() {
     println!("u8最大值: {}, 最小值{}", u8::max_value(), u8::min_value());
-    lib::show_me();
+    lib::helper::show_me();
 }
