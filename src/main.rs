31f201e866fec8c1402869dfd2c8e9b4387d50c9
wxvirus <1418667580@qq.com>
2022-09-04 11:51:55 +0800

    函数和字符串以及表达式

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-mod lib; // lib.rs 或者 lib/mod.rs
-fn main() {
-    println!("u8最大值: {}, 最小值{}", u8::max_value(), u8::min_value());
-    lib::helper::show_me();
+// mod lib; // lib.rs 或者 lib/mod.rs
 
-    lib::config::show_version();
+fn get_user(uid:i32)->&'static str {
+    let ret = if uid == 1 {
+        "wujie"
+    } else if uid == 2 {
+        "张三"
+    } else {
+        "unknown"
+    };
+    ret
+}
+
+fn add (i:i32)->i32 {
+    i+1
+}
+
+fn main() {
+    println!("user is {}", get_user(3));
+
+    let a = 1; // 语句
+    let b = if a == 1 {5}else {10};
+    println!("{}", b);
+
+    println!("{}", add(3));
 }
