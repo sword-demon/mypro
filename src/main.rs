c933e31d79eff4d1aa1d25e94dafa6beb531ea63
wxvirus <1418667580@qq.com>
2022-09-10 20:00:57 +0800

    rust 所有者转移和修改原字符串

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // mod lib; // lib.rs 或者 lib/mod.rs
 
+fn change(s: &mut String) {
+   s.push_str("_19");
+}
+
 fn main() {
-   let name = String::from("abc");
-   let you = name.clone();
-   println!("{:p}", name.as_ptr());
-   println!("{:p}", you.as_ptr());
+   let mut name = String::from("abc");
+   change(&mut name);
+   println!("{}", name);
 }
