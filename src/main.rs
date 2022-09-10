246d880a085faee2c40421476b21ba987d981eba
wxvirus <1418667580@qq.com>
2022-09-10 17:48:32 +0800

    rust string

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
 // mod lib; // lib.rs 或者 lib/mod.rs
 
-fn get_user(uid:i32)->&'static str {
-    let ret = if uid == 1 {
-        "wujie"
-    } else if uid == 2 {
-        "张三"
-    } else {
-        "unknown"
-    };
-    ret
-}
-
-fn add (i:i32)->i32 {
-    i+1
-}
-
-fn filter(html:&str) {
-   for i in 1..=10  {
-       println!("{}", i);
-   }
-}
 fn main() {
-    println!("user is {}", get_user(3));
+   let first_name = String::from("无");
+   let last_name = String::from("解");
 
-    let a = 1; // 语句
-    let b = if a == 1 {5}else {10};
-    println!("{}", b);
+//    let name = first_name + &last_name; // &str &String => &str 自动转化
+//    println!("{}", name);
 
-    println!("{}", add(3));
-
-    let html = "abcd";
-    filter(html);
+   let name1 = format!("{}{}", first_name, last_name);
+   println!("{}", name1);
 }
