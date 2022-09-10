c1f413b23175449a7ed2e003c1bde3a3cbb18dfb
wxvirus <1418667580@qq.com>
2022-09-10 19:43:17 +0800

    rust 所有者

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 // mod lib; // lib.rs 或者 lib/mod.rs
 
 fn main() {
-   let first_name = String::from("无");
-   let last_name = String::from("解");
-
-//    let name = first_name + &last_name; // &str &String => &str 自动转化
-//    println!("{}", name);
-
-   let name1 = format!("{}{}", first_name, last_name);
-   println!("{}", name1);
+   let name = String::from("abc");
+   let you = name.clone();
+   println!("{:p}", name.as_ptr());
+   println!("{:p}", you.as_ptr());
 }
