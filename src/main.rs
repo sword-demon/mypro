01497403769ddbc6e3cd60ab2511eb579a0f1c0a
wxvirus <1418667580@qq.com>
2022-10-16 15:32:07 +0800

    实体类修改函数

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use models::user_model::{self, UserInfo};
+
 // mod lib; // lib.rs 或者 lib/mod.rs
 mod models;
 
+fn set_user(u: &mut models::user_model::UserInfo) {
+    u.user_id = 101;
+    u.user_name = String::from("wujie");
+    u.user_age = 10;
+    u.user_tags = ["java", "php", "js", "go", "rust"];
+}
+
 fn main() {
-  let user = models::UserModel::new_user_model();
-  println!("{:#?}", user);
+    let mut user = models::user_model::new_user_info();
+    set_user(&mut user);
+    println!("{:?}", user);
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,1 +1,1 @@
-pub mod UserModel;
+pub mod user_model;
