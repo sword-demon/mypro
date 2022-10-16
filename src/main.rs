c3245ec850eca58d6a572a2bd393c6fd92c3f2a5
wxvirus <1418667580@qq.com>
2022-10-16 19:28:27 +0800

    struct impl 泛型

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use models::user_model::{self, UserInfo};
+use models::user_model::{new_user_info, new_user_score_a, new_user_score_b, UserInfo};
 
 // mod lib; // lib.rs 或者 lib/mod.rs
 mod models;
 
-fn set_user(u: &mut models::user_model::UserInfo) {
+fn set_user(u: &mut UserInfo) {
     u.user_id = 101;
     u.user_name = String::from("wujie");
     u.user_age = 10;
@@ -11,7 +11,21 @@ fn set_user(u: &mut models::user_model::UserInfo) {
 }
 
 fn main() {
-    let mut user = models::user_model::new_user_info();
+    let mut user = new_user_info();
     set_user(&mut user);
     println!("{:?}", user);
+
+    let mut user_score = new_user_score_a();
+    user_score.user_id = 101;
+    user_score.score = 10;
+    println!("{:?}", user_score);
+    println!("{:?}", user_score.get_user_type());
+    println!("{:?}", user_score.get_user_id());
+
+    let mut user_score_b = new_user_score_b();
+    user_score_b.user_id = "#EFff";
+    user_score_b.score = 10.0;
+    println!("{:?}", user_score_b);
+    println!("{:?}", user_score_b.get_user_type());
+    println!("{:?}", user_score_b.get_user_id());
 }
--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -14,3 +14,38 @@ pub fn new_user_info() -> UserInfo {
         user_tags: [""; 5],
     }
 }
+
+#[derive(Debug)]
+pub struct UserScore<A, B> {
+    // 泛型
+    pub user_id: A,
+    pub score: B,
+    pub comment: &'static str,
+}
+
+impl<A, B> UserScore<A, B> {
+    pub fn get_user_type(&self) -> &'static str {
+        // 表达式
+        &self.comment
+    }
+
+    pub fn get_user_id(&self) -> &A {
+        &&self.user_id
+    }
+}
+
+pub fn new_user_score_a() -> UserScore<i32, i32> {
+    UserScore {
+        user_id: 0,
+        score: 0,
+        comment: "A类用户",
+    }
+}
+
+pub fn new_user_score_b() -> UserScore<&'static str, f32> {
+    UserScore {
+        user_id: "",
+        score: 0.0,
+        comment: "B类用户",
+    }
+}
