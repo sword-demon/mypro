d8e83ae0d7086a822bac7c527e599b65757b858c
wxvirus <1418667580@qq.com>
2022-10-25 00:04:11 +0800

    rust trait 创建实体类

--- src/api/prods.rs
+++ src/api/prods.rs
@@ -5,11 +5,16 @@ use crate::models::book_model::Book;
 
 pub trait Prods {
     fn get_price(&self) -> f32;
+    fn new(id: i32, price: f32) -> Self;
 }
 
 // 使用 trait 对某一个 struct 定义方法
 impl Prods for Book {
     fn get_price(&self) -> f32 {
         &self.price + 10.0
+    }
+
+    fn new(id: i32, price: f32) -> Book {
+        Book { id, price }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use api::Prods;
 use models::book_model::*;
 
 fn main() {
-    let book = new_book(101, 25.0);
+    // 需要指明具体的类型
+    let book: Book = Prods::new(101, 25.0);
     println!("{:?}", book.get_price());
 }
