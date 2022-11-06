6138b950e4eb4292530c1ab697b547325546625e
wxvirus <1418667580@qq.com>
2022-11-06 15:35:59 +0800

    生命周期入门

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 mod api;
 mod models;
-use api::Prods;
-use api::Stock;
-use models::book_model::*;
+// use api::Prods;
+// use api::Stock;
+// use models::book_model::*;
 
-fn sum(p1: Book, p2: Book) {
-    println!(
-        "p1的价格是: {}, p2的价格是: {}, 商品总价是: {}",
-        p1.get_price(),
-        p2.get_price(),
-        p1 + p2
-    );
+fn max<'a>(a: &'a i32, b: &'a i32) -> &'a i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
-    // 需要指明具体的类型
-    let book1: Book = Prods::new(101, 25.6);
-    let book2: Book = Prods::new(110, 30.5);
-    sum(book1, book2);
+    let a = 12;
+    let b = 21;
+    println!("最大值是: {}", max(&a, &b));
 }
