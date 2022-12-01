d2917ea1171f03f18fb89374f87fbee5f6f552cc
wxvirus <1418667580@qq.com>
2022-12-01 23:26:32 +0800

    所有权深入自定义实现Clone

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 #[macro_use]
 mod mymacros;
+#[derive(Debug, Default)]
+struct User {
+    id: i32,
+    name: String,
+    age: i32,
+}
+
+impl Clone for User {
+    fn clone(&self) -> Self {
+        User {
+            id: self.id,
+            name: self.name.clone() + "被克隆",
+            age: self.age,
+        }
+    }
+}
+
 fn main() {
     func!(php);
     php();
+
+    let a = User {
+        id: 123,
+        ..Default::default()
+    };
+    let b = a.clone();
+    println!("a: {:?}, b: {:?}", a, b);
 }
